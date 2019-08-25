f2830bfb017611d83fc30a1f15998d0508bf9afd
Alexander <gcastilloalex@gmail.com>
2019-08-25 01:47:54 -0500

    CRUD for Category

--- app/Model/Category.php
+++ app/Model/Category.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+
+    // cambiar el id por el slug
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
 }
