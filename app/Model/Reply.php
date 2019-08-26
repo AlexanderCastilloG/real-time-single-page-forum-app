d489cac61a2a0542eb8d512dfd217a8018b85925
Alexander <gcastilloalex@gmail.com>
2019-08-26 10:38:01 -0500

    CRUD for Reply

--- app/Model/Reply.php
+++ app/Model/Reply.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+
     public function question() {
         return $this->belongsTo(Question::class);
     }
